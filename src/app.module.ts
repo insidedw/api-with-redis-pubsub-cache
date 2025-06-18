1ad62ce8017ab047d59bf6288d0a1467a661bb29
dk <insidedw@gmail.com>
2025-06-18 09:03:39 +0900

    apply local cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      ttl: 60000, // Cache items for 1 minute
+      max: 100, // Maximum number of items in cache
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getHello(): Promise<string> {
+    const cachedValue = await this.cacheManager.get<string>('hello');
+
+    if (cachedValue) {
+      console.log('cachedValue', cachedValue);
+      return cachedValue;
+    }
+    console.log('cachedValue not found');
+
+    const value = 'Hello World!';
+    await this.cacheManager.set('hello', value);
+    return value;
   }
 }
